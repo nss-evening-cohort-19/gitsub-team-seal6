e9f0c9b7d51d83d5343572aad360a42971421968
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-05-15 21:37:44 -0500

    Fixed form header

--- scripts/main.js
+++ scripts/main.js
@@ -189,7 +189,7 @@ const repoCard = () => {
 
 const formInput = () => {
   let domString = `<form class="mb-3">
-  <label for="name" class="form-label">Project board name</label>
+  <label for="name" class="form-label">Pinned Repositories</label>
   <input type="text" class="form-control" id="name" placeholder="Repo name">
   </div>
   <div class="mb-3">
